aa0fb6b9db6cb8c86e20f499d115f2b8f869960e
Mohamed A. Tolba <tolba96mohamed@gmail.com>
2019-04-25 16:22:02 +0200

    Add predict function

--- RMDL/rmdl_text.py
+++ RMDL/rmdl_text.py
@@ -31,20 +31,18 @@ from RMDL import plot as plt
 from RMDL import score
 
 
-def evaluate_model(x_test, y_test, model_filepath, weights_filepath,
-                    batch_size=128, sparse_categorical=True):
+def predict_single_model(x_test, model_filepath, weights_filepath,
+                            batch_size=128, sparse_categorical=True):
     with open(model_filepath, "r") as model_file:
         model = model_from_json(model_file.read())
     model.load_weights(weights_filepath)
     if sparse_categorical:
         y_pred = model.predict_classes(x_test, batch_size=batch_size)
         y_pred = np.array(y_pred)
-        acc_score = accuracy_score(y_test, y_pred)
     else:
         y_pred = model.predict(x_test, batch_size=batch_size)
         y_pred = np.argmax(y_pred, axis=1)
-        acc_score = accuracy_score(np.argmax(y_test, axis=1), y_pred)
-    return y_pred, acc_score
+    return y_pred
 
 
 def train(x_train, y_train, x_val,  y_val, batch_size=128,
@@ -322,6 +320,105 @@ def train(x_train, y_train, x_val,  y_val, batch_size=128,
     if plot:
         plt.plot_history(history)
 
+
+def predict(x_test, batch_size=128, max_seq_len=500, max_num_words=75000,
+                sparse_categorical=True, random_deep=[3, 3, 3],
+                models_dir="models", weights_dir="weights",
+                tf_idf_vectorizer_filepath="tf_idf_vectorizer.pickle",
+                text_tokenizer_filepath="text_tokenizer.pickle"):
+    """
+    predict(x_test, batch_size=128, max_seq_len=500, max_num_words=75000,
+                    sparse_categorical=True, random_deep=[3, 3, 3],
+                    models_dir="models", weights_dir="weights",
+                    tf_idf_vectorizer_filepath="tf_idf_vectorizer.pickle",
+                    text_tokenizer_filepath="text_tokenizer.pickle")
+
+    Parameters
+    ----------
+        batch_size: int, optional
+            Number of samples per gradient update. It will default to 128.
+        max_seq_len: int, optional
+            Maximum number of words in a text to consider. It will default to 500.
+        max_num_words: int, optional
+            Maximum number of unique words in datasets. It will default to 75000.
+        sparse_categorical: bool
+            When target's dataset is (n,1) should be True. It will default to True.
+        random_deep: array of int [3], optional
+            Number of ensembled models used in RMDL random_deep[0] is number of DNNs,
+            random_deep[1] is number of RNNs, random_deep[2] is number of CNNs. It will default to [3, 3, 3].
+        models_dir: string, optional
+            Path to the directory where the pre-trained models are saved. It will default to "models".
+        weights_dir: string, optional
+            Path to the directory where the weights of the pre-trained models are saved. It will default to "weights".
+        tf_idf_vectorizer_filepath: string, optional
+            Path to tf-idf vectorizer used in preprocessing while training RMDL. It will default to "tf_idf_vectorizer.pickle".
+        text_tokenizer_filepath: string, optional
+            Path to text tokenizer used in preprocessing while training RMDL. It will default to "text_tokenizer.pickle".
+
+    Returns
+    -------
+        y_pred: list
+            List of the final predictions made by the ensemble using majority voting.
+        models_y_pred: dictionary
+            Dictionary of the predictions made by each individual model of the ensemble.
+            Example: models_y_pred["DNN-0"] = [...]
+
+    Raises
+    ------
+        IOError: When the open operation on any of the required files (in function parameters) fails.
+    """
+    models_y_pred = {}
+
+    if random_deep[0] != 0:
+        x_test_tf_idf = txt.get_tf_idf_vectors(x_test,
+                                                max_num_words=max_num_words,
+                                                fit=False,
+                                                vectorizer_filepath=tf_idf_vectorizer_filepath)
+    if random_deep[1] != 0 or random_deep[2] != 0:
+        x_test_tokenized, _ = txt.tokenize(x_test,
+                                            max_num_words=max_num_words,
+                                            max_seq_len=max_seq_len,
+                                            fit=False,
+                                            tokenizer_filepath=text_tokenizer_filepath)
+
+    del x_test
+    gc.collect()
+
+    for i in range(len(random_deep)):
+        for j in range(random_deep[i]):
+            try:
+                print(f"\nPredicting Using {util.model_type[i]}-{j}")
+                model_file = f"{util.model_type[i]}_{j}.json"
+                weights_file = f"{util.model_type[i]}_{j}.hdf5"
+                model_filepath = os.path.join(models_dir, model_file)
+                weights_filepath = os.path.join(weights_dir, weights_file)
+                if i == 0:
+                    x_test = x_test_tf_idf
+                else:
+                    x_test = x_test_tokenized
+                y_pred = predict_single_model(x_test,
+                                                model_filepath,
+                                                weights_filepath,
+                                                batch_size=batch_size,
+                                                sparse_categorical=sparse_categorical)
+                models_y_pred[f"{util.model_type[i]}-{j}"] = y_pred
+            except Exception as e:
+                print(f"Error in {util.model_type[i]}-{j}\n")
+                print(f"Check the Error \n {e}")
+
+    del x_test
+    del x_test_tf_idf
+    del x_test_tokenized
+    gc.collect()
+
+    y_probs = np.array(models_y_pred.values()).transpose()
+    y_pred = []
+    for i in range(y_probs.shape[0]):
+        sample_pred = np.array(y_probs[i, :])
+        sample_pred = collections.Counter(sample_pred).most_common()[0][0]
+        y_pred.append(sample_pred)
+    return y_pred, models_y_pred
+    
 
 def predict_evaluate(x_test, y_test, batch_size=128, max_seq_len=500, max_num_words=75000,
                         sparse_categorical=True, random_deep=[3, 3, 3], plot=False, models_dir="models",
